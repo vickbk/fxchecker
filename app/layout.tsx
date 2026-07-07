cc05cc9963b7c5e20179dbb1ab42a1f1c035b015
vickbk <vickbake8@gmail.com>
2026-07-07 12:32:53 +0200

    fix: Correctly use HeadingCtx as a component instead of a provider in RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <HeadingCtx.Provider value={0}>{children}</HeadingCtx.Provider>
+        <HeadingCtx value={0}>{children}</HeadingCtx>
       </body>
     </html>
   );
