460d1c16aa1c07554423ab844485a8b8f8f89df6
vickbk <vickbake8@gmail.com>
2026-07-07 12:28:13 +0200

    feat: Add Vitest imports for testing and enable client-side rendering in HeadingCtx

--- shared/heading/components/heading-managers.test.tsx
+++ shared/heading/components/heading-managers.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import { describe, expect, test } from "vitest";
 import { checkHeadingOrder, drawRegion } from "../scripts";
 import { Article, Heading, Main, Section } from "./heading-managers";
 
