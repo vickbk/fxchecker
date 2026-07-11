f7bac1a9dcee530238c1cf0c611a2f1c04e9572a
vickbk <vickbake8@gmail.com>
2026-07-11 08:59:47 +0200

    feat: add EmptySection component and integrate it into EmptyCompare, EmptyFavorite, EmptyHistory, and EmptyLogs

--- shared/utils/components/index.ts
+++ shared/utils/components/index.ts
@@ -1,5 +1,6 @@
 export { BiIcon } from "./bi-icon";
 export { CurrencyCard } from "./CurrencyCard";
+export { EmptySection } from "./EmptySection";
 export { Flag } from "./Flag";
 export { LoadingPlacehoder } from "./LoadingPlaceholder";
 export { SectionsWrapper } from "./SectionsWrapper";
