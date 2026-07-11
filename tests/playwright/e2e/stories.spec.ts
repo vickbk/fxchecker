57a6130792ac7b70f7095c2c68dad84767a998a3
vickbk <vickbake8@gmail.com>
2026-07-11 14:49:02 +0200

    refactor: update Playwright tests to use PAGE_TITLE constant and simplify test cases

--- tests/playwright/e2e/stories.spec.ts
+++ tests/playwright/e2e/stories.spec.ts
@@ -1,20 +1,9 @@
-import { test, expect } from "@playwright/test";
+import { PAGE_TITLE } from "@/tests/common/utils/constants";
+import { test } from "@playwright/test";
+import { shouldSee } from "../utils/dsl";
 
 test("has title", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
+  await page.goto("/");
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test("get started link", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
-
-  // Click the get started link.
-  await page.getByRole("link", { name: "Get started" }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(
-    page.getByRole("heading", { name: "Installation" }),
-  ).toBeVisible();
+  await shouldSee(page, PAGE_TITLE);
 });
