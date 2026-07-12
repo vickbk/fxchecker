7c589250931b79014b7887ecd1448a317ad796a4
vickbk <vickbake8@gmail.com>
2026-07-13 00:27:44 +0200

    feat: update SWAPPER_TEXT regex for improved pattern matching

--- features/converter/__testing__/utils.ts
+++ features/converter/__testing__/utils.ts
@@ -1,7 +1,7 @@
 export const CONVERTER_TITLE = /Check the rate/i;
 export const SEND_HEADER = /Send rate/i;
 export const RECEIVE_HEADER = /Receive Rate/i;
-export const SWAPPER_TEXT = /Swap send and receive currencies/i;
+export const SWAPPER_TEXT = /(Swap send)(.*?)(and receive)(.*?)(currencies)/i;
 export const AMOUNT_LABEL = /Exchange amount/i;
 export const CURRENT_RATE = /Current rate for/i;
 export const FAVORITE_BUTTON = /Add to favorite/i;
