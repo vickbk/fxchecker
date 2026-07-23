da59220f7ac6e56e738a46325175eb100f46daa0
vickbk <vickbake8@gmail.com>
2026-07-23 12:03:32 +0200

    feat: update CurrencyCard and CurrencyPicker components for improved accessibility and user interaction

--- features/converter/components/CurrencyCard.tsx
+++ features/converter/components/CurrencyCard.tsx
@@ -1,9 +1,15 @@
 "use client";
 import { useCurrencies } from "@/shared/currencies";
-import { Article, Heading, Section } from "@/shared/heading";
-import { BiIcon, Flag, getCurrencyCountry, SROnly } from "@/shared/utils";
+import { Article } from "@/shared/heading";
+import {
+  BiIcon,
+  Flag,
+  getCurrencyCountry,
+  LoadingPlaceholder,
+  SROnly,
+} from "@/shared/utils";
 import { SRHidden } from "@/shared/utils/components/SRHidden";
-import { useId } from "react";
+import { useId, useState } from "react";
 import { useCurrencyFilter } from "../hooks/useCurrencyFilter";
 import { useURLState } from "../hooks/useURLState";
 
@@ -18,6 +24,7 @@ export const CurrencyCard = ({ isSend = false }: { isSend: boolean }) => {
     useCurrencyFilter({
       currencies,
     });
+  const [choice, setChoice] = useState(isSend ? from : to);
 
   const actualCurr =
     currencies.find(({ code }) => code === (isSend ? from : to)) ??
@@ -25,6 +32,7 @@ export const CurrencyCard = ({ isSend = false }: { isSend: boolean }) => {
 
   if (!actualCurr) return null;
   const country = getCurrencyCountry(actualCurr!.code);
+  const popoverElement = document.getElementById(popover);
 
   return (
     <Article id={`${id}`}>
@@ -44,63 +52,75 @@ export const CurrencyCard = ({ isSend = false }: { isSend: boolean }) => {
         aria-live="polite"
         onToggle={(e) => {
           if (e.newState === "open") openMenu();
-          else closeMenu();
+          else {
+            (isSend ? setFrom : setTo)(choice);
+            closeMenu();
+          }
         }}
         className={`bg-btn inset-auto [position-anchor:--${popover}] [position-area:bottom_span-left] [position-try:flip-block] mt-4 p-4 rounded-lg text-foreground`}
       >
-        {isOpen && (
-          <>
-            <label className="relative w-full">
-              <SROnly>Enter the currency you like</SROnly>{" "}
-              <BiIcon name="search absolute left-2 top-[.005em]" />
-              <input
-                type="text"
-                id={searchId}
-                autoFocus
-                onChange={(e) => setQuery(e.target.value)}
-                placeholder="Search for currencies..."
-                className="pl-8 outline outline-foreground-secondary rounded-sm p-2 w-full"
-              />
-            </label>
-
-            <Section>
-              <Heading className="flex justify-between border-b border-foreground-secondary py-4 text-foreground-secondary">
-                Currencies <span>{filteredCurrencies.length}</span>
-              </Heading>
-              <ul className="mt-4 max-h-[40vh] overflow-y-auto scrollbar-none">
+        <label className="relative w-full">
+          <SROnly>Enter the currency you like</SROnly>{" "}
+          <BiIcon name="search absolute left-2 top-[.005em]" />
+          <input
+            type="text"
+            id={searchId}
+            autoFocus
+            onChange={(e) => setQuery(e.target.value)}
+            placeholder="Search for currencies..."
+            className="pl-8 outline outline-foreground-secondary rounded-sm p-2 w-full"
+          />
+        </label>
+        <fieldset>
+          <legend className="w-full flex justify-between border-b border-foreground-secondary py-4 text-foreground-secondary">
+            Currencies <span> {filteredCurrencies.length}</span>
+          </legend>
+          <div className="mt-4 max-h-[40vh] min-h-[30vh] w-64 p-1 overflow-y-auto scrollbar-none">
+            {!isOpen ? (
+              <LoadingPlaceholder className="h-[30vh] bg-card" />
+            ) : (
+              <>
                 {filteredCurrencies.map(({ name, code }) => (
-                  <li key={code}>
-                    <button
-                      type="button"
-                      popoverTarget={popover}
-                      onClick={() => {
-                        (isSend ? setFrom : setTo)(code);
+                  <label
+                    key={code}
+                    className="relative rounded-lg flex gap-2 text-sm text-foreground-secondary items-center p-2 w-full hover:outline hover:outline-foreground-secondary has-focus-visible:outline has-focus-visible:outline-lime-500 action-btn"
+                  >
+                    <input
+                      type="radio"
+                      name="convert-currency"
+                      className="absolute scale-0 opacity-0"
+                      onChange={(e) => {
+                        if (e.target.checked) {
+                          setChoice(code);
+                        }
                       }}
-                      className="flex gap-2 text-sm text-foreground-secondary items-center p-2 w-full"
-                    >
-                      <Flag
-                        src={`https://flagcdn.com/${getCurrencyCountry(code)}.svg`}
-                        alt=""
-                      />{" "}
-                      <SRHidden className="text-lg text-foreground">
-                        {code}
-                      </SRHidden>{" "}
-                      <span className="truncate max-w-48">{name}</span>
-                      {code === actualCurr.code && (
-                        <BiIcon name="check text-lg ml-auto" />
-                      )}
-                    </button>
-                  </li>
+                      onKeyDown={(e) => {
+                        if ([" ", "Enter"].includes(e.key))
+                          popoverElement?.hidePopover();
+                      }}
+                      onClick={(e) => {
+                        if (e.detail !== 0) popoverElement?.hidePopover();
+                      }}
+                    />
+                    <Flag alt="" country={getCurrencyCountry(code)} />{" "}
+                    <SRHidden className="text-lg text-foreground">
+                      {code}
+                    </SRHidden>{" "}
+                    <span className="truncate max-w-48">{name}</span>
+                    {(code === actualCurr.code || code === choice) && (
+                      <BiIcon name="check text-lg ml-auto" />
+                    )}
+                  </label>
                 ))}
                 {filteredCurrencies.length === 0 && (
-                  <li className="flex h-full items-center justify-center text-sm">
+                  <label className="flex h-full items-center justify-center text-sm">
                     No results found
-                  </li>
+                  </label>
                 )}
-              </ul>
-            </Section>
-          </>
-        )}
+              </>
+            )}
+          </div>
+        </fieldset>
       </form>
     </Article>
   );
