080197685f61bb1ab8a606b7f25470eb0175c916
vickbk <vickbake8@gmail.com>
2026-07-28 16:52:48 +0200

    refactor: update History Skeleton to remove the menue when data is not yet ready

--- shared/utils/modules/navbar/components/OptionMenue.tsx
+++ shared/utils/modules/navbar/components/OptionMenue.tsx
@@ -8,13 +8,13 @@ export const OptionMenue = ({ text, children }: OptionProps) => {
   const { isActive, isHistory, queryString } = useActiveOption(text);
   return (
     <li
-      className={`p-4 border-b-2 hover:border-lime-500 has-focus-visible:outline has-focus-visible:outline-lime-500 has-focus-visible:border-transparent has-focus-visible:rounded-lg ${isActive ? "border-lime-500" : "border-transparent"}`}
+      className={`p-4 relative border-b-2 hover:border-lime-500 has-focus-visible:outline has-focus-visible:outline-lime-500 has-focus-visible:border-transparent has-focus-visible:rounded-lg ${isActive ? "border-lime-500" : "border-transparent"}`}
     >
       <Link
         className="block focus-visible:outline-none"
         href={`/${isHistory ? "" : text}${queryString && "?" + queryString}`}
       >
-        {text} {children}
+        {text} {children} <span className="absolute inset-0"></span>
       </Link>
     </li>
   );
