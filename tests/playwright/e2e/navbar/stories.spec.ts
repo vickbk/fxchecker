b45c1137247510a0e8312cb705a0329efda4246c
vickbk <vickbake8@gmail.com>
2026-08-09 01:09:09 +0200

    refactor: remove unused script exports and add navigation bar heading level tests

---
+++ tests/playwright/e2e/navbar/stories.spec.ts
@@ -0,0 +1,13 @@
+import { shouldHaveCorrectLevelHeading } from "@/shared/heading/__testing__/stories";
+import { test } from "@playwright/test";
+
+test.describe("Navigation bar", () => {
+  ["/", "/compare", "/features", "/logs"].forEach((path) =>
+    test(`heading levels should be correct navigating to "${path}" `, async ({
+      page,
+    }) => {
+      await page.goto(path);
+      await shouldHaveCorrectLevelHeading(page);
+    }),
+  );
+});
